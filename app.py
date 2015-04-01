fdcf25ffe914c9bc144e53b6e92d949b54e08e19
Kevin Coxe <kevcoxe@gmail.com>
2015-03-31 22:03:02 -0400

    ok now let's generate from some data

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    posts = [
+        {'title': 'How to start a flask app', 'content': 'Here is some info about how to start creating flask apps'},
+        {'title': 'Add to a form', 'content': "Let's add something to this!"},
+        ]
+    return render_template('index.html',posts=posts)
 
 if __name__ == '__main__':
     app.run(
